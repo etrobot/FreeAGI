e07e6e7960e459bc892a87efa04c1b23d2ed4af6
tfproduct01 <etrobot@outlook.com>
2023-04-19 10:09:33 +0800

    add config.ini

--- app.py
+++ app.py
@@ -10,19 +10,14 @@ import logging
 
 def plan(cmd: str,steps:list=None):
     # prepare command
-    promptTemplate=',output the breif steps or key points in Mermaid format. Reply in language '+LANG
+    promptTemplate=' help me make ideas and output in Mermaid format,dont write any program code. Reply in language '+LANG
     if cmd == config['cmd']['cmd']:
         prompt0 = 'I started a project, final goal:%s.'%cmd + promptTemplate
     else:
-        missons=''.join(str(steps.index(x)+1)+'.'+x.replace(']',';').split('[')[-1] for x in steps)
-        prompt0 = 'Final Goal:%s'%config['cmd']['cmd']+'\nKey points:%s'%missons + '\nSub-misson:' + cmd[1:] + '\nFor the Sub-misson '+ promptTemplate
+        prompt0 = 'Final Goal:%s'%config['cmd']['cmd'] + '\nFocus on the point %s%s'%(steps.index(cmd)+1,cmd[1:]) + promptTemplate
 
     # generate plan in mermaid format
     logger.info('asking New Bing about 『%s』...' % prompt0)
-    if 'sys' in config.keys() and 'proxy' in config['sys'].keys():
-        bingBot = bingChat(cookies=cookies, proxy=config['sys']['proxy'])
-    else:
-        bingBot = bingChat(cookies=cookies)
     response = asyncio.run(bingBot.ask(prompt=prompt0, conversation_style=ConversationStyle.creative,
                             wss_link="wss://sydney.bing.com/sydney/ChatHub"))
     replyTxt:str=response["item"]["messages"][1]["adaptiveCards"][0]["body"][0]["text"].replace('graph LR','graph TD')
@@ -44,7 +39,7 @@ def plan(cmd: str,steps:list=None):
     }
     matches = re.findall(r'[a-zA-Z]+\[.*?\]', mermaid_content)
     for row in [x for x in matches if cmd not in x]:
-        if(len(row))>8:
+        if(len(row))>12:
             mermaidJson['steps'][row] = {}
     t.sleep(14)
     return mermaidJson
@@ -80,4 +75,8 @@ if __name__ == '__main__':
     logger=logging.getLogger(__name__)
     with open('./cookies.json', 'r') as f:
         cookies = json.load(f)
+        if 'sys' in config.keys() and 'proxy' in config['sys'].keys():
+            bingBot = bingChat(cookies=cookies, proxy=config['sys']['proxy'])
+        else:
+            bingBot = bingChat(cookies=cookies)
         build_tree(3, config['cmd']['cmd'])
