a1b5fb5af7533a05e988b0daeae07c5d651bc53e
tfproduct01 <etrobot@outlook.com>
2023-04-23 10:20:06 +0800

    improve extension

--- browser extension/script.js
+++ browser extension/script.js
@@ -1,7 +1,7 @@
 // 0:wait2start  1:wait4gen
 var statusCode
 var tasks
-var taskNum=0
+var taskNum = 0
 var goal
 
 const rootEl = document.querySelector('div[id="__next"]')
@@ -42,9 +42,9 @@ async function addBar() {
     runBtn.addEventListener("click", buildPrompt);
     var stopBtn = bar.querySelector("#stopauto");
     stopBtn.addEventListener("click", stopAuto);
-    if(tasks!=undefined)
-        bar.querySelector('#task').innerHTML='Next Question: '+tasks[taskNum+1]
-    if(!document.querySelector('polygon')){
+    if (tasks != undefined)
+        bar.querySelector('#task').innerHTML = 'Next Question: ' + tasks[taskNum + 1]
+    if (!document.querySelector('polygon')) {
         bar.querySelector('#autorun').style.display = "none"
         bar.querySelector('.loader').style.display = "block"
     }
@@ -57,7 +57,7 @@ function checkTasks() {
     mermaidCode = document.querySelector('code');
     if (mermaidCode) {
         var tasksFromMermaid = convertMermaid(mermaidCode.innerHTML);
-        if (tasks!=undefined && JSON.stringify(tasks) == JSON.stringify(tasksFromMermaid)) {
+        if (tasks != undefined && JSON.stringify(tasks) == JSON.stringify(tasksFromMermaid)) {
             statusCode = clearInterval(statusCode)
             console.log(tasks)
             buildPrompt();
@@ -67,83 +67,90 @@ function checkTasks() {
     }
 }
 
-function stopAuto(){
-    statusCode=clearInterval(statusCode)
-    taskNum=tasks.length
-    bar.querySelector('#task').innerHTML='Next Question: '+tasks[taskNum+1]
+function stopAuto() {
+    statusCode = clearInterval(statusCode)
+    var stopGenBtn = document.querySelector("#__next > div.overflow-hidden.w-full.h-full.relative.flex > div.flex.h-full.max-w-full.flex-1.flex-col > main > div.absolute.bottom-0.left-0.w-full.border-t.md\\:border-t-0.dark\\:border-white\\/20.md\\:border-transparent.md\\:dark\\:border-transparent.md\\:bg-vert-light-gradient.bg-white.dark\\:bg-gray-800.md\\:\\!bg-transparent.dark\\:md\\:bg-vert-dark-gradient.pt-2 > form > div > div:nth-child(1) > div > button")
+    if (stopGenBtn && stopGenBtn.textContent == 'Stop generating') stopGenBtn.click()
+    document.querySelector('#stopauto').style.display = "none"
+    document.querySelector("#autorun").style.display = "block"
     const textArea = document.querySelector("textArea")
-    textArea.value=''
+    textArea.value = ''
+    document.querySelector('.loader').style.display = "none"
+    taskNum += 1
 }
 
 function convertMermaid(mermaidCode) {
     var regex = /[\(\)\[\]\{\}][^\(\)\[\]\{\}]*[\(\)\[\]\{\}]/g;
     const matches = mermaidCode.match(regex);
-    if(!matches)return false
+    if (!matches) return false
     const results = [];
-    const regexGraph= /\s*graph[\s\S]*/; 
-    const cleanTxt=mermaidCode.replace(regexGraph,'')
-    results.push(cleanTxt+'\nPlease analyze the point: '+matches[0])
+    const regexGraph = /\s*graph[\s\S]*/;
+    const cleanTxt = mermaidCode.replace(regexGraph, '')
+    results.push(cleanTxt + '\nPlease analyze the point: ' + matches[0])
     for (let i = 1; i < matches.length; i++) {
         const match = matches[i];
         const result = match.substring(1, match.length - 1);
-        if(result!=goal && result.length>7)
-            results.push(' next point: '+result);
+        if (result != goal && result.length > 7)
+            results.push(' next point: ' + result);
     }
     return results
 }
 
 
 function convertMarkdown(txt) {
-    var regex = /^\d+\.\s(.+)/gm;
+    var regex = /^\d+\.(.+)$/gm;
     const matches = txt.match(regex);
-    if(!matches){
-        tasks= convertMermaid(txt)
+    if (!matches) {
+        tasks = convertMermaid(txt)
         return tasks
     }
-    tasks=[]
-    tasks.push(txt.replace(regex, '')+matches[0])
+    tasks = []
+    tasks.push(txt.replace(regex, '') + matches[0])
     for (let i = 1; i < matches.length; i++) {
         tasks.push(matches[i])
     }
     return true
 }
 
-function task(){
-    if(document.querySelector('polygon')){
-        statusCode=clearInterval(statusCode)
-        if(tasks!=undefined && taskNum<tasks.length)
-            taskNum+=1;
-            buildPrompt()
+function task() {
+    if (document.querySelector('polygon')) {
+        statusCode = clearInterval(statusCode)
+        if (tasks != undefined && taskNum < tasks.length)
+            taskNum += 1;
+        buildPrompt()
+        document.querySelector('.loader').style.display = "block"
     }
 }
 
 function buildPrompt() {
     const textArea = document.querySelector("textArea");
-    if(!textArea)return
-    const promptTemplate = '. Help me figure out key points for the goal and output key points in Mermaid format,dont write any program code.';
-    if (tasks==undefined) {
-        if(textArea.value=='')return
-        if(convertMarkdown(textArea.value)){
-            textArea.value=tasks[taskNum]
-            statusCode=setInterval(task,5000)
+    if (!textArea) return
+    const promptTemplate = '. Help me figure out key points and output key points in Mermaid format,dont write any program code.';
+    if (tasks == undefined) {
+        if (textArea.value == '') return
+        if (convertMarkdown(textArea.value)) {
+            textArea.value = tasks[taskNum]
+            statusCode = setInterval(task, 5000)
         }
-        else{
+        else {
             goal = textArea.value;
             textArea.value = 'Final Goal:' + goal + promptTemplate
-            statusCode = setInterval(checkTasks, 5000)}
-    }else {
-        if(taskNum==tasks.length || tasks[taskNum]==undefined){
-            textArea.value=='';
-            tasks=undefined;
-            taskNum=0;
+            statusCode = setInterval(checkTasks, 5000)
+        }
+    } else {
+        if (taskNum == tasks.length || tasks[taskNum] == undefined) {
+            textArea.value == '';
+            tasks = undefined;
+            taskNum = 0;
             document.querySelector('.loader').style.display = "none"
-            return}
-        if(goal==undefined){
-            textArea.value =tasks[taskNum]
-        }else
-            {textArea.value = 'Final Goal:' + goal  + '. Focus on ' + tasks[taskNum] + promptTemplate}
+            return
+        }
+        if (goal == undefined) {
+            textArea.value = tasks[taskNum]
+        } else { textArea.value = 'Final Goal:' + goal + '. Focus on ' + tasks[taskNum] + promptTemplate }
         document.querySelector('#autorun').click()
-        document.querySelector('#task').innerHTML='Next Question: '+tasks[taskNum+1]
-        statusCode=setInterval(task,5000)
+        document.querySelector('#task').innerHTML = 'Next Question: ' + tasks[taskNum + 1]
+        document.querySelector('#stopauto').style.display = 'block'
+        statusCode = setInterval(task, 5000)
     }
 }
